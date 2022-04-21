9f9abbc6070a07495b166a6498fb64564e035917
Хохлова Наталия <nataliebasket@gmail.com>
2022-04-21 18:48:27 +0300

    оптимизация

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -11,22 +13,22 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: '[name].[contenthash].js',
-    clean: true,
+    assetModuleFilename: 'img/[name][ext]',
+    // clean: true,
   },
   devServer: {
-    port: 3001
+    // contentBase: path.join(__dirname, 'build'),
+    port: 3001,
+    open: true
   },
+  // доп. функционал к базовой комплектации
   plugins: [
     new HTMLWebpackPlugin({
       template: './index.html',
-      inject: 'body'
+      inject: 'body',
     }),
     new CopyWebpackPlugin({
       patterns: [
-        {
-          from: path.resolve(__dirname, 'src/img/'),
-          to: path.resolve(__dirname, 'build/img/')
-        },
         {
           from: path.resolve(__dirname, 'src/data/data.json'),
           to: path.resolve(__dirname, 'build/data/')
@@ -42,20 +44,62 @@ module.exports = {
       ]
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css'
+      filename: '[name].[contenthash].css',
     }),
   ],
   module: {
     rules: [
       {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
+      {
+        test: /\.(?:png|jpg|jpeg|svg)$/,
+        type: 'asset/resource',
+        use: [
+              {
+                loader: ImageMinimizerPlugin.loader,
+                options: {
+                  minimizer: {
+                    implementation: ImageMinimizerPlugin.imageminMinify,
+                    options: {
+                      plugins: [
+                        "imagemin-gifsicle",
+                        "imagemin-jpegtran",
+                        "imagemin-pngquant",
+                        "imagemin-svgo"
+                      ],
+                    },
+                  },
+                },
+              }
+            ],
+      },
+      //["imagemin-pngquant", {quality: [0.3, 0.3]}],
     ],
   },
   optimization: {
     minimizer: [
       new CssMinimizerPlugin(),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          // Implementation
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          // Options
+          options: {
+            plugins: [
+              "imagemin-gifsicle",
+              "imagemin-jpegtran",
+              "imagemin-pngquant",
+              "imagemin-svgo",
+            ],
+          },
+        },
+      }),
     ],
   }
 }
